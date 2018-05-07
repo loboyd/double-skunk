66f7e90e7d3b5a50a52defa06a8112bed0e88ae9
loboyd <logan.boyd@protonmail.com>
2018-05-06 22:55:16 -0400

    add built-in support for ints, floats, and int lists

--- src/peer.py
+++ src/peer.py
@@ -12,6 +12,7 @@ def send(addr, send_data):
     host = addr[0]
     port = addr[1]
 
+    send_data = convert_to_string(send_data)
     s = socket.socket()
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(('0.0.0.0', port))
@@ -24,9 +25,6 @@ def recv(addr):
     """Receive a piece of data from a peer. The function assumes the other
     peer is waiting to send data. THIS WILL HANG IF DATA IS NOT RECEIVED
     SUCCESSFULLY; TIMEOUT SHOULD BE IMPLEMENTED AT SOME POINT"""
-    host = addr[0]
-    port = addr[1]
-
     received = 0
 
     while not received:
@@ -43,12 +41,14 @@ def recv(addr):
 
         s.close()
 
+    recv_data = convert_from_string(recv_data)
     return recv_data
 
 def exchange(addr, send_data):
     """Exchange two pieces of data with socket connections. This function
     assumes both peers call it at approximately the same time. Otherwise,
     it will hang (should probably implement a timeout in the future)."""
+    send_data = convert_to_string(send_data)
 
     host = addr[0]
     port = addr[1]
@@ -73,6 +73,7 @@ def exchange(addr, send_data):
             s.close()
 
         # try being the server
+        # send(addr, send_data)
         if not sent:
             s = socket.socket()
             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -83,7 +84,36 @@ def exchange(addr, send_data):
             c.close()
             sent = 1
 
+    recv_data = convert_from_string(recv_data)
     return recv_data
+
+def convert_to_string(data):
+    """Convert data into a string to be sent over socket connection
+    Supports strings, ints, floats, and lists of ints"""
+    data_type = type(data)
+    if data_type is str:
+        return 's' + data
+    elif data_type is int:
+        return 'i' + str(data)
+    elif data_type is float:
+        return 'f' + str(data)
+    elif data_type is list:
+        return 'l' + ' '.join(map(str, data))
+    else:
+        raise TypeError('unsupported type')
+
+def convert_from_string(data):
+    """Convert data from socket sendable string into original data type
+    Supports strings, ints, floats, and lists of ints"""
+    type_flag = data[0]
+    if type_flag is 's':
+        return data[1:]
+    elif type_flag is 'i':
+        return int(data[1:])
+    elif type_flag is 'f':
+        return float(data[1:])
+    elif type_flag is 'l':
+        return map(int, data[1:].split(' '))
 
 def get_peer_ip():
     """Get IP address of a friend. (DO SOME SANITY CHECKING HERE.)
